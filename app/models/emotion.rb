16d2f844f146d42ca96d3206df460299cfc73526
Oscar Lee <owlee@umass.edu>
2017-03-01 13:21:40 -0500

    Refactoring table and adding important info first

--- app/models/emotion.rb
+++ app/models/emotion.rb
@@ -13,4 +13,21 @@ class Emotion < ActiveRecord::Base
     self.sadness ||= 0.0
     self.surprise ||= 0.0
   end
+
+  # Returns a sorted hash of all emotions
+  def hashify
+    emotions = {anger: anger, contempt: contempt, disgust: disgust, fear: fear,
+    happiness: happiness, neutral: neutral, sadness: sadness, surprise: surprise}
+    emotions.sort_by {|k, v| v}.reverse.to_h
+  end
+
+  def primary_score
+    h = self.hashify
+    [h.keys[0], h.values[0]]
+  end
+
+  def secondary_score
+    h = self.hashify
+    [h.keys[1], h.values[1]]
+  end
 end
