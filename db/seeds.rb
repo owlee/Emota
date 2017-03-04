8e544d7294f6b89b7b30d70267d8d8898ba7218e
Oscar Lee <owlee@umass.edu>
2017-03-04 02:35:00 -0500

    Oops dont want to send text when seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 puts 'Seeding Emota Image Set'
 Dir.glob((Rails.application.root+'db/emota_image_set/*.jpg').to_s) do |image_path|
-  Emotum.build image_path, 1, 1
+  Emotum.build image_path, 0, 1
 end
 puts 'Seeding Emota Image Set has complete'
