15c9679c18a0cb4956f5ff34a6e98868416cf5e9
Oscar Lee <owlee@umass.edu>
2017-04-03 01:47:08 -0400

    Editing Seeds to account for conversations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,20 @@
-puts 'Seeding Emota Image Set'
-puts Time.now
+puts '##### Seeding Emota Image Set #####'
+
+# Simulate Conversations
+dir = Rails.application.root + 'db/emota_image_set/'
+
+count = Dir[File.join(dir, '**', '*')].count { |file| File.file?(file) }
+
 Emotum.destroy_all
-Dir.glob((Rails.application.root+'db/emota_image_set/*.jpg').to_s) do |image_path|
-  Emotum.build image_path, 0, 1
+(0...count).each do |minute|
+  if count % 10 == 0
+    puts '### Ending conversation ###'
+    sleep(1.minute)
+    puts '### Waking up to new conversation ###'
+  end
+  puts Time.now
+  Dir.glob((dir + '*.jpg').to_s) { |image_path| Emotum.build image_path, 0, 1 }
 end
+
 puts Time.now
-puts 'Seeding Emota Image Set has complete'
+puts '##### Seeding Emota Image Set has complete #####'
